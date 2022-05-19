3025ab75845bf65732b8ce7e5811f385dd6919ed
Husanboy <husanboysobirjonov02@gmail.com>
2022-05-19 23:02:36 +0500

    initial

---
+++ src/main/java/com/shubh/kafkachat/consumer/MessageListener.java
@@ -0,0 +1,34 @@
+package com.shubh.kafkachat.consumer;
+
+import com.shubh.kafkachat.constants.KafkaConstants;
+import com.shubh.kafkachat.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MessageListener {
+    @Autowired
+    SimpMessagingTemplate template;
+
+    List<Message> messages = new ArrayList<>(10);
+
+    @KafkaListener(
+            topics = KafkaConstants.KAFKA_TOPIC,
+            groupId = KafkaConstants.GROUP_ID
+    )
+    public void listen(Message message) {
+        messages.add(message);
+        System.out.println(messages.size());
+        System.out.println(messages);
+        if (messages.size() == 10) {
+            template.convertAndSend("/topic/group", messages);
+            messages.clear();
+        }
+
+    }
+}
---
+++ src/main/java/com/shubh/kafkachat/model/Message.java
@@ -0,0 +1,68 @@
+package com.shubh.kafkachat.model;
+
+public class Message {
+    private String sender;
+    private String content;
+    private String timestamp;
+    private String longitude;
+    private String latitude;
+
+    public Message() {
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "sender='" + sender + '\'' +
+                ", content='" + content + '\'' +
+                ", timestamp='" + timestamp + '\'' +
+                ", longitude='" + longitude + '\'' +
+                ", latitude='" + latitude + '\'' +
+                '}';
+    }
+}
